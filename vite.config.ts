b918473bbbf1608df59d56f5d53bfe2190363c6a
cloris-cc <clorislli@tencent.com>
2025-02-25 11:43:09 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Linklink/',
+  base: '/Linklinklink/' // 👈 这里设置仓库名称
 })
