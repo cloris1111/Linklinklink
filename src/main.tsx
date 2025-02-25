bc8cef6eadb820819b6574bb12806199ae3f0d66
cloris1111 <clorislli@tencent.com>
2025-02-25 13:21:23 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Linklink">
+    <BrowserRouter basename="/Linklinklink">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
